50baa9f7b421d3e158dd388e720dd938b97ddb4a
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 20:21:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,10 @@ var ingredient3 = "sharp cheese"
 var ingredient4 = "butter"
 var ingredient5 = "tomato"
 var ingredient6 = "garlic"
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
