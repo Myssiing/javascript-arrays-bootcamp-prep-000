acc77c6f91e3cd1ce4160319893aa5c252ddfa76
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 22:44:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ var chocolateBars = [
   'skittles'
 ];
 
-const array = [1]
+/*const array = [1]
 const element = 'foo'
 
 function addElementToBeginningOfArray(array, element) {
@@ -63,3 +63,39 @@ function removeElementFromEndOfArray(array) {
   array = array.slice(0, array.length - 1)
   return array
 }
+*/
+
+
+ function addElementToBeginningOfArray(array, element){
+   array = [element, ...array]
+   return array;
+ }
+ function destructivelyAddElementToBeginningOfArray(array, element){
+   array.unshift(element);
+   return array;
+ }
+ function addElementToEndOfArray(array, element){
+   array = [...array, element]
+   return array;
+ }
+ function destructivelyAddElementToEndOfArray(array, element){
+   array.push(element);
+   return array;
+ }
+ function accessElementInArray(array, index){
+   return array[index];
+ }
+ function destructivelyRemoveElementFromBeginningOfArray(array){
+    array.shift()
+    return array
+ }
+ function removeElementFromBeginningOfArray(array){
+   return array.slice(1)
+ }
+ function destructivelyRemoveElementFromEndOfArray(array){
+   array.pop()
+   return array
+ }
+ function removeElementFromEndOfArray(array){
+   return array.slice(0, array.length -1)
+ }
