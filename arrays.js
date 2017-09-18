5090254b1622bac5d908cb9b95bd3b02539bd35a
Tara Foulkrod <photochik02@hotmail.com>
2017-09-18 22:21:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,5 +43,5 @@ function accessElementInArray() {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   var array = [1, 2, 3]
   array.shift()
-  return array.shift
+  return array
 }
